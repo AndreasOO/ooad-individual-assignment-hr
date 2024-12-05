fcb188e106e21ac1d1af2f81da76dea3d8257405
Andreas Ohlander <andreas.ohlander@gmail.com>
2024-12-05 11:21:57 +0100

    Added search methods for database

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,11 @@ public class Main {
 //        gui.init();
 
     Database database = new Database();
-    database.getEmployees().entrySet().forEach(e -> {System.out.println("ID: " + e.getKey() + "  Name: " + e.getValue().getName());});
+//    database.getEmployees().entrySet().forEach(e -> {System.out.println("ID: " + e.getKey() + "  Name: " + e.getValue().getName());});
+
+//        database.searchByID(1).forEach(e -> System.out.println(e.getName()));
+
+//        database.searchByName("Andy").forEach(e -> System.out.println(e.getName()));
+        database.emptySearch().forEach(e -> System.out.println(e.getName()));
  }
 }
--- src/Model/Database.java
+++ src/Model/Database.java
@@ -17,6 +17,24 @@ public class Database {
         return DataLoader.loadData().stream().collect(Collectors.toMap(Employee::getId, Function.identity()));
     }
 
+    public List<Employee> emptySearch() {
+        return employees.values().stream().toList();
+    }
+
+    public List<Employee> searchByName(String searchInput) {
+        return employees.values().stream().filter(x -> x.getName().contains(searchInput.trim())).collect(Collectors.toList());
+    }
+
+    public List<Employee> searchByID(long id) {
+        List<Employee> result =  new ArrayList<>();
+
+        if (employees.containsKey(id)) {
+            result.add(employees.get(id));
+        }
+        return result;
+    }
+
+
     public Map<Long, Employee> getEmployees() {
         return employees;
     }
